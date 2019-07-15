28efe4eba302ff76b94bbc2c39ec529814a1a578
Zac Kracht <kracht22@vt.edu>
2019-07-15 16:18:15 -0400

    Added unit tests for budget class

--- budget.hpp
+++ budget.hpp
@@ -13,74 +13,77 @@ Description: Header file containing interface for a budget than can be created a
 #include <string>
 #include <map>
 
-//Interface for creating and modifying a monthly-based budget
+/**
+Class: Budget
+Description: Interface for creating and modifying a monthly-based budget
+*/
 class Budget
 {
 public:
-	//default constructor
+	///default constructor
 	Budget() {};
 
-	//constructor with initial income
+	///constructor with initial income
 	Budget(double income);
 
-	//constructor with initial income, expenses, and weights
+	///constructor with initial income, expenses, and weights
 	Budget(double income, const std::map<std::string, double>& expenses, const std::map<std::string, double>& weights);
 	
-	//copy constructor
+	///copy constructor
 	Budget(const Budget& x);
 	
-	//destructor
+	///destructor
 	~Budget();
 	
-	//copy assignment
+	///copy assignment
 	Budget& operator=(const Budget& x);
 
-	// inequality comparison for two budgets, needed for = operator
+	/// inequality comparison for two budgets, needed for = operator
 	friend bool operator!=(const Budget & left, const Budget & right) noexcept;
 
-	// equality comparison for two budgets
+	/// equality comparison for two budgets
 	friend bool operator==(const Budget & left, const Budget & right) noexcept;
 
-	//Add amount to the income value
+	///Add amount to the income value
 	void addIncome(double amount) noexcept;
 	
-	//add an expense with cost and name to the budget
+	///add an expense with cost and name to the budget
 	void addExpense(std::string name, double cost);
 	
-	//add a weight of remaining income to the budget
+	///add a weight of remaining income to the budget
 	void addWeight(std::string name, double weight) noexcept;
 	
-	//get the associated cost by the name of an item
+	///get the associated cost by the name of an item
 	double getExpense(std::string name) const;
 
-	//get the associated weight by the name of an item
+	///get the associated weight by the name of an item
 	double getWeight(std::string name) const;
 
-	//get the map of weights
+	///get the map of weights
 	std::map<std::string, double> getWeights() const noexcept;
 
-	//get the map of expenses
+	///get the map of expenses
 	std::map<std::string, double> getExpenses() const noexcept;
 	
-	//getter function for income variable
+	///getter function for income variable
 	double getIncome() const noexcept;
 	
-	//getter function for remaining variable
+	///getter function for remaining variable
 	double getRemaining() const noexcept;
 
-	//getter function for total weights
+	///getter function for total weights
 	double getTotalWeight() const noexcept;
 	
-	//remove an expense with name from the map of expenses
+	///remove an expense with name from the map of expenses
 	void removeExpense(std::string name);
 	
-	//remove weight with name from the map of weights
+	///remove weight with name from the map of weights
 	void removeWeight(std::string name);
 
-	//convert all the weights to monetary values based on remaining income, return as a map from name to value
+	///convert all the weights to monetary values based on remaining income, return as a map from name to value
 	std::map<std::string, double> weightsToValues() noexcept;
 	
-	//printing function to visualize/debug budget
+	///printing function to visualize/debug budget
 	void print(std::ostream & out) noexcept;
 	
 
--- budget_test.cpp
+++ budget_test.cpp
@@ -11,25 +11,120 @@ Description: Unit testing file for Budget class using Qt's QTest framework
 class BudgetTest : public QObject {
 	Q_OBJECT
 	private slots:
-		void ConstructorTest();
+		void initTestCase();
+		void IncomeOnlyConstructorTest();
+		void MainConstructorTest();
+		void CopyConstructorTest();
+		void EqualOperatorTest();
+		void ComparisonTest();
+		void AddIncomeTest();
+		void AddExpenseTest();
+		void AddWeightsTest();
+		void GetExpenseTest();
+		void GetWeightTest();
 	private:
+		Budget test_a;
+		Budget test_b;
+		Budget test_c;
+		Budget test_d;
+
+		std::map<std::string, double> expenses;
+		std::map<std::string, double> weights;
 };
 
-void BudgetTest::ConstructorTest() {
+void BudgetTest::initTestCase() {
 	double income = 1000.0;
-	std::map<std::string, double> expenses;
-	std::map<std::string, double> weights;
 	expenses["car payment"] = 250.0;
 	expenses["spotify"] = 5.0;
 	weights["food"] = 25.0;
 	weights["clothes"] = 10.0;
 	weights["savings"] = 65.0;
 
-	Budget test(income, expenses, weights);
-	QVERIFY(test.getIncome() == 1000.0);
-	QVERIFY(test.getRemaining() == 745.0);
-	QVERIFY(test.getTotalWeight() == 100.0);
+	test_a = Budget(income);
+	test_b = Budget(income, expenses, weights);
+	test_c = Budget(test_b);
+	test_d = test_c;
 }
+
+void BudgetTest::IncomeOnlyConstructorTest() {
+	QVERIFY(test_a.getIncome() == 1000.0);
+	QVERIFY(test_a.getRemaining() == 1000.0);
+	QVERIFY(test_a.getTotalWeight() == 0.0);
+}
+
+void BudgetTest::MainConstructorTest() {
+	QVERIFY(test_b.getIncome() == 1000.0);
+	QVERIFY(test_b.getRemaining() == 745.0);
+	QVERIFY(test_b.getTotalWeight() == 100.0);
+}
+
+void BudgetTest::CopyConstructorTest() {
+	QVERIFY(test_c.getIncome() == 1000.0);
+	QVERIFY(test_c.getRemaining() == 745.0);
+	QVERIFY(test_c.getTotalWeight() == 100.0);
+	QVERIFY(test_c.getExpenses() == expenses);
+	QVERIFY(test_c.getWeights() == weights);
+}
+
+void BudgetTest::EqualOperatorTest() {
+	QVERIFY(test_c.getIncome() == 1000.0);
+	QVERIFY(test_c.getRemaining() == 745.0);
+	QVERIFY(test_c.getTotalWeight() == 100.0);
+	QVERIFY(test_c.getExpenses() == expenses);
+	QVERIFY(test_c.getWeights() == weights);
+}
+
+void BudgetTest::ComparisonTest() {
+	QVERIFY(test_a != test_b);
+	QVERIFY(test_b == test_c);
+	QVERIFY(test_c == test_d);
+}
+
+void BudgetTest::AddIncomeTest() {
+	test_a.addIncome(100.0);
+	QVERIFY(test_a.getIncome() == 1100.0);
+	QVERIFY(test_a.getRemaining() == 1100.0);
+	QVERIFY(test_a.getTotalWeight() == 0.0);
+}
+
+void BudgetTest::AddExpenseTest() {
+	test_b.addExpense("insurance", 50.0);
+	test_b.addExpense("cable", 125.50);
+
+	QVERIFY(test_b.getIncome() == 1000.0);
+	QVERIFY(test_b.getRemaining() == 569.5);
+	QVERIFY(test_b.getTotalWeight() == 100.0);
+	QVERIFY(test_b.getExpense("cable") == 125.5);
+	QVERIFY(test_b.getExpense("insurance") == 50.0);
+	QVERIFY_EXCEPTION_THROWN(test_b.addExpense("bad", 600.0), std::logic_error);
+}
+
+void BudgetTest::AddWeightsTest() {
+	test_c.addWeight("gym", 5.0);
+	test_c.addWeight("college", 8.3);
+
+	QVERIFY(test_c.getIncome() == 1000.0);
+	QVERIFY(test_c.getRemaining() == 745.0);
+	QVERIFY(test_c.getTotalWeight() == 113.3);
+	QVERIFY(test_c.getWeight("gym") == 5.0);
+	QVERIFY(test_c.getWeight("college") == 8.3);
+}
+
+void BudgetTest::GetExpenseTest() {
+	QVERIFY(test_b.getExpense("car payment") == 250.0);
+	QVERIFY(test_b.getExpense("spotify") == 5.0);
+	QVERIFY_EXCEPTION_THROWN(test_b.getExpense("bad"), std::range_error);
+
+}
+
+void BudgetTest::GetWeightTest() {
+	QVERIFY(test_b.getWeight("food") == 25.0);
+	QVERIFY(test_b.getWeight("clothes") == 10.0);
+	QVERIFY(test_b.getWeight("savings") == 65.0);
+	QVERIFY_EXCEPTION_THROWN(test_b.getWeight("bad"), std::range_error);
+}
+
+
 
 QTEST_MAIN(BudgetTest)
 #include "budget_test.moc"
